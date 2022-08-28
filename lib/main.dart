7c4b5e12622f79c59a459415e58d842dacd90399
Diseirang <diseirang.chheun@mjqeducation.edu.kh>
2022-08-28 20:49:05 +0700

    Change Title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'My Monthlt Report'),
+      home: const MyHomePage(title: 'My Monthly Report'),
     );
   }
 }
