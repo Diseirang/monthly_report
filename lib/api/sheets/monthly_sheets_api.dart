2fbb8fc07ea30c4d9ac4d773267605cda53c6d65
Diseirang <diseirang.chheun@mjqeducation.edu.kh>
2022-08-29 22:28:28 +0700

    GSheetsApi

---
+++ lib/api/sheets/monthly_sheets_api.dart
@@ -0,0 +1,18 @@
+class MonthlySheetsApi {
+  static const _credentails = r'''
+{
+  "type": "service_account",
+  "project_id": "monthly-report-360915",
+  "private_key_id": "1327e1906e8c3ec43965fdacbdad20e35cd4d682",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDeM4AM5ky/vtTV\nL9amtvZf2V3EaFnMSidKNdFrXfaH0EO3KeLXXbSWIVbEiGZSRDuvIuqnkPsJXGr0\nubcAeBj8H5nHtsGha7Q539aqjSXWwTgR5P7xc8wrBYbS7aGRZvyVXwNFfo3UdXf5\nQkZe/yp3lrTdck4s1oHYd/ny7RsrpNiXjfW9aPS/t2qco2g0GLW99hlyq9aGIQXV\nrpS/PQvSf99I3Frfkq5Vp4BEi3Jq9OZaoKoOSk8keUY+UB5EJNZXFajMpbWdETsL\nIrPSwU2wNc470K1A85/IMf03gkn1kisOs4eCQ2nJYXuxtYk6vzAYLxor85DT7O4I\nnEJ6RfiTAgMBAAECggEAEFjem1RYIiUyeEqwMoy0XIHb3abFsHtmH5YNBaagjAZc\n7P1GZIzOy9rBDO1yBaxibbSWaeSHK32imsWM+ppbUDUs9Z3y6eLIHBflLZ/7wCxv\n6RUdUnsv8pxR+nOkFtPqUjzuuy6f1Zkczfg7wCJqp+is/ru5l5/iZqARAffCr3eq\no+3Y2bgVlhBB0Ex5YH2DH68VfHv9LcZ2K/BpF2E0p2sMOUrEdyJ+XRQ5L6BPlzU8\nivOcLdpwc1hYIvtNl4dpqtNM//AYWqM5jIEM7ir0AsqkdSp33Ujx/LbH9a+LMbnQ\nO5DbV/igvpTypeHPqjWOx9mq5B1ROHIE9OxWVrbDvQKBgQDzTjIdxuED1/ZvwgLC\nXK4pmrL8Zu1FpddnT5iH8x8knS+fsKMpAHFOD19tGAFDfVWBPFk4tHWoQA4uKcb2\nySoCgvorAr7wcz42c1YWtYQy4iGa+XuKXnyWSuYbiUeHdDDvVdXkh0MZzm+DkeKR\nP91ihrpLkeWlcCrh6FiKGI4GjQKBgQDpy2q42HS0SfZY77DwVhukNMXMjKlfRVjw\nqvTj5fwvi1gSjTThYk4VI/orclWARQAfJ6s6lbdTusaa6rMfUAZ4tDmorVuJyk0g\nsePz4yHAQ5/NOD+T8CWuKB3x2ZTy6gfZZUnnN6NHE1vh8H3apMaeXwSu3Ls0mC1V\nAK8DKf1DnwKBgG7IzzcRxIlsf5C5pWn8q2zUPOWCtE0/S0qGSqy0fhy82d97nX9g\n7Tae91OEV6b0Xcefw/x25w/6QrqwWSErzc4g92o4MNrpjo1ajPuAITbOEoEvUhpa\nFZ6Vchtdsc0ykcmYqvEIyfvpLIubFLAQhWyEbjVUtF1VKqF8mzweHCVNAoGBALjs\n22eGXxlT/3VtcilIEukZjPM0t6vR8H2826OAAK+iqnFSG+fCmDyEjpbbAuJIscXy\n3K+kSLQD4qAa0MANznCfLZRMO0GMMOGRk9sPZ1zqoHX07eUYaNWOidEI55YUo0zD\nn2cyTQYlO+JFFWdiOnB+e/fMRI9fZ0v7FHOOPPPFAoGAc15sG4ZzMoJR7k2QfBOy\nNCjN9qKLf1lLbDbWImYm3Eg8u8pj7gsgztduuhibuWww0ZFNAR5ZU0S/CKlq7x2/\nCGrTOwMscqWYQXJN4kIO/0SNeNxGz9Gqihc0i+FsqrJ0Yg/XGCNkNIH5XoddfG2f\nrbCmBxEKEsaUmg1t0ZUplOo=\n-----END PRIVATE KEY-----\n",
+  "client_email": "monthly-report@monthly-report-360915.iam.gserviceaccount.com",
+  "client_id": "104115310017385951459",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/monthly-report%40monthly-report-360915.iam.gserviceaccount.com"
+}
+
+''';
+  static final _spreadSheetID = '1zf4g5nk3fKZOEtSxINusdQI21dwt8N7Dxo9Trl2gfdo';
+}
